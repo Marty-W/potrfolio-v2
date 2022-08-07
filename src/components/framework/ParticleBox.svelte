70187605c6e25564fc96eb7b1227464a501d64a9
Martin Weber <martin.weber@01people.com>
2022-08-07 21:12:37 +0200

    added tabs to second screen, new const, new svelte comp

---
+++ src/components/framework/ParticleBox.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Particles from 'svelte-particles'
+	import { loadFull } from 'tsparticles'
+	import config from '../../data/particles.json'
+
+	let particlesInit = async engine => {
+		// you can use main to customize the tsParticles instance adding presets or custom shapes
+		// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+		// starting from v2 you can add only the features you need reducing the bundle size
+		await loadFull(engine)
+	}
+</script>
+
+<Particles id="tsparticles" options={config} {particlesInit} />
+
+<style>
+	:global(#tsparticles) {
+		height: 100%;
+	}
+</style>
