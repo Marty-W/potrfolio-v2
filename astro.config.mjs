c221fbf8eadc0df65acf5aaab30786d889afd926
Martin Weber <martin.weber@01people.com>
2022-08-07 14:42:53 +0200

    migrate particles to svelte, update pckgs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [tailwind(), react(), svelte()]
 });
